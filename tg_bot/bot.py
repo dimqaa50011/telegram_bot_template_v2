3ed1fffc8433e8957fbb0358b0150278ab7e8f67
Dmitry <dz5011@yandex.ru>
2023-03-02 12:32:02 +0300

    create executable file from bot

---
+++ tg_bot/bot.py
@@ -0,0 +1,48 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
+from loguru import logger
+
+from config import BOT_TOKEN, USE_REDIS
+
+
+def register_all_middlewares(dp: Dispatcher):
+    pass
+
+
+def register_all_filters(dp: Dispatcher):
+    pass
+    
+    
+def register_all_handlers(dp: Dispatcher):
+    pass
+
+
+async def on_startup():
+    bot = Bot(token=BOT_TOKEN, parse_mode='HTML')
+    storage = RedisStorage2() if USE_REDIS else MemoryStorage()
+    dp = Dispatcher(bot=bot, storage=storage)
+    
+    register_all_middlewares(dp)
+    register_all_filters(dp)
+    register_all_handlers(dp)
+    
+    bot_session = await bot.get_session()
+    
+    logger.info('Bot started!!')
+    
+    try:
+        await dp.start_polling()
+    finally:
+        await storage.close()
+        await storage.wait_closed()
+        await bot_session.close()
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(on_startup())
+    except (SystemError, KeyboardInterrupt) as ex:
+        logger.info('Bot stopped!')
