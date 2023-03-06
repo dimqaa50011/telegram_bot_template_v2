9095dca0d277fa06904b471073ed17a4683758ec
Dmitry <dz5011@yandex.ru>
2023-03-06 08:05:05 +0300

    tg user schema created

---
+++ tg_bot/schemas/user.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field
+
+
+class UserSchemaBase(BaseModel):
+    first_name: str = Field(default=None)
+    last_name: str = Field(default=None)
+    tg_username: str = Field(default=None)
+    tg_id: int = Field(...)
+
+
+class CreateUserSchema(UserSchemaBase):
+    pass
+
+
+class OutUserSchema(UserSchemaBase):
+    pass
